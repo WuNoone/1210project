ed12c960d4e9583a1a82fb93e2e30057b3e0d260
WuNoone <42664235+WuNoone@users.noreply.github.com>
2018-12-09 23:26:25 +0800

    Update first.component.ts

--- perinf/src/app/first/first.component.ts
+++ perinf/src/app/first/first.component.ts
@@ -7,32 +7,34 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponent implements OnInit {
   constructor(
   ) { }
+  data = {
+    name: '',
+    sex: '男',
+    age: '',
+    tel: '',
+    address: '',
+  };
   saveDate(name: string, sex: string, age: number, tel: number, address: string): void {
-    // tslint:disable-next-line:prefer-const
-    let namevalue = (document.getElementsByName('name')[0] as HTMLInputElement).value;
-    // tslint:disable-next-line:prefer-const
-    let sexvalue = (document.getElementsByName('sex')[0] as HTMLInputElement).value;
-    // tslint:disable-next-line:prefer-const
-    let agevalue = (document.getElementsByName('age')[0] as HTMLInputElement).value;
-    // tslint:disable-next-line:prefer-const
-    let telvalue = (document.getElementsByName('tel')[0] as HTMLInputElement).value;
-    // tslint:disable-next-line:prefer-const
-    let addressvalue = (document.getElementsByName('address')[0] as HTMLInputElement).value;
+    const userInformation = {
+      name: this.data.name,
+      sex: this.data.sex,
+      age: age,
+      tel: tel,
+      address: address,
+    };
+    const namevalue = (document.getElementsByName('name')[0] as HTMLInputElement).value;
+    const sexvalue = (document.getElementsByName('sex')[0] as HTMLInputElement).value;
+    const agevalue = (document.getElementsByName('age')[0] as HTMLInputElement).value;
+    const telvalue = (document.getElementsByName('tel')[0] as HTMLInputElement).value;
+    const addressvalue = (document.getElementsByName('address')[0] as HTMLInputElement).value;
     if (namevalue === '' || sexvalue === '' || agevalue === '' || telvalue === '' || addressvalue === '') {
       alert('内容不能为空！');
     } else {
-      const userInformation = {
-        name: name,
-        sex: sex,
-        age: age,
-        tel: tel,
-        address: address,
-      };
       location.reload();
         localStorage.setItem('information', JSON.stringify(userInformation));
         console.log(userInformation);
     }
-    }
+  }
 
   ngOnInit() {
   }
