1db314804a27015d23b21b702b2cd992530dab05
WuNoone <1033951807@qq.com>
2018-12-13 20:31:16 +0800

    fix:练习5修改

--- perinf/src/app/practice/pra-three/pra-three.component.ts
+++ perinf/src/app/practice/pra-three/pra-three.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 import { ParThreeServiceService } from './par-three-service.service';
 
 @Component({
@@ -7,37 +7,20 @@ import { ParThreeServiceService } from './par-three-service.service';
   templateUrl: './pra-three.component.html',
   styleUrls: ['./pra-three.component.css']
 })
-export class PraThreeComponent implements OnInit {
+export class PraThreeComponent implements OnInit, OnDestroy {
 
   public date = new Date();
-  // public num = 0;
-  // public start = '开始';
+
   constructor(private threeServer: ParThreeServiceService) { }
 
-  // private continueSubscription: Subscription;
-
-  // onBtnStart() {
-  //   if (this.start === '开始') {
-  //     this.start = '暂停';
-  //     this.continueSubscription = interval(1000).subscribe(() => this.num++);
-  //   } else if (this.start === '暂停') {
-  //     this.start = '继续';
-  //     this.continueSubscription.unsubscribe();
-  //   } else if (this.start === '继续') {
-  //     this.start = '暂停';
-  //     this.continueSubscription = interval(1000).subscribe(() => this.num++);
-  //   }
-  // }
-
-  // onBtnStop() {
-  //   this.num = 0;
-  //   this.start = '开始';
-  //   this.continueSubscription.unsubscribe();
-  // }
+  private timingSubscription: Subscription;
 
   ngOnInit() {
-    const datetime = interval(1000);
-    datetime.subscribe(() => this.date = new Date());
+    this.timingSubscription = interval(1000).subscribe(() => this.date = new Date());
+  }
+
+  ngOnDestroy() {
+    this.timingSubscription.unsubscribe();
   }
 
 }
